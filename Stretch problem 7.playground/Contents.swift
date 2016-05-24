ebf6e4bc31e78848bbe6e42e567bec1e71cee9c3
Eva <Bresciano>
2016-05-24 10:11:51 -0600

    finished

--- Stretch problem 7.playground/Contents.swift
+++ Stretch problem 7.playground/Contents.swift
@@ -7,13 +7,32 @@ var str = "Hello, playground"
 
 protocol Vehicle {
     var speed: Double {get}
-    var isMoving: Bool {get}
+    var isMoving: Bool {get set}
     
-    func startMoving() -> Bool
+    mutating func startMoving()
     
-    func stopMoving() -> Bool
+    mutating func stopMoving()
     
 }
+
+extension Vehicle {
+    
+   mutating func startMoving() {
+        if !isMoving {
+            isMoving = true
+            print(isMoving)
+            
+        }
+    }
+       mutating func stopMoving() {
+            if isMoving {
+                isMoving = false
+                print(isMoving)
+            }
+        }
+    }
+
+    
 
 
 class Truck: Vehicle {
@@ -26,18 +45,48 @@ class Truck: Vehicle {
         
     }
     
-    func startMoving() -> Bool {
-        if isMoving == true {
-          return true
-        
-        }
     
-    func stopMoving() -> Bool {
-        if isMoving == false {
-            return false
+
+}
+
+class LawnMower: Vehicle {
+var speed: Double
+var isMoving: Bool
+
+init(speed: Double, isMoving: Bool) {
+    self.speed = speed
+    self.isMoving = isMoving }
+    
+    
+    func startMoving() {
+        if !isMoving {
+            isMoving = true
+            print(isMoving)
+            
         }
     }
+        func stopMoving() {
+            if isMoving {
+                isMoving = false
+                print(isMoving)
+            }
+        }
+
 }
 
 
-}
+
+
+
+var pickup = Truck(speed: 0.0, isMoving: false)
+
+print(pickup.isMoving)
+pickup.startMoving()
+
+
+
+
+
+
+
+
